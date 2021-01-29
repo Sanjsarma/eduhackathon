88363c1c32d7f614668e2ffd7001f92e73121802
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2021-01-30 01:16:18 +0530

    'Userdashboard'

--- db.sql
+++ db.sql
@@ -7,3 +7,23 @@ role VARCHAR(30),
 college VARCHAR(200), 
 sem VARCHAR(10),
 primary key(id))ENGINE=InnoDB;
+
+create table branch ( id int primary key,
+bname varchar(250)
+);
+
+alter table courses add link varchar (200);
+
+create table allotment (
+id INTEGER AUTO_INCREMENT,
+ktuid varchar(20) references user(ktuid),
+courseid varchar(7) references courses(courseid),
+unique(ktuid,courseid) ,
+primary key(id));
+
+create table courses ( courseid varchar(7) ,
+cname varchar(250),
+sem int(2),
+branchid int, 
+foreign key (branchid) references branch(id),
+primary key (courseid));
